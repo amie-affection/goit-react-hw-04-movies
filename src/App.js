d340d1313cc722a00b5e47ee40af76f40efcf5aa
amie-affection <amie.affection@gmail.com>
2020-07-01 21:42:55 +0300

    added MovieDetailsPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./header/Header";
 import HomePage from "./homePage/HomePage";
 import MoviePage from './moviePage/MoviePage';
+import MovieDetailsPage from './movieDetailsPage/MovieDetailsPage';
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/movies" component={MoviePage} />
-          {/* <Route path="/books/:title" component={OneBookPage} /> */}
+          <Route path="/books/:movieId" component={MovieDetailsPage} />
         </Switch>
       </BrowserRouter>
     );
--- src/movieDetailsPage/MovieDetailsPage.jsx
+++ src/movieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-
+import React, { Component } from "react";
+import { getInfoAboutFilm } from "../services/api";
 
 class MovieDetailsPage extends Component {
-    state = {  }
-    render() {
-        return 1;
-    }
+  state = {
+    poster: "",
+    info: "",
+    error: null,
+  };
+
+  componentDidMount() {
+    // console.log(this.props.match.params);
+    const { info } = this.props.match.params;
+
+    getInfoAboutFilm(info)
+      .then((data) => {
+        //   console.log(data)
+        this.setState({ poster: data.results.poster_path, info: data.results.title });
+      })
+      .catch((error) => this.setState({ error }));
+  }
+
+  render() {
+    const { poster, info } = this.state;
+
+    return (
+      <>
+            <p>{poster}</p>
+            <p>{info}</p>
+      </>
+    );
+  }
 }
 
 export default MovieDetailsPage;
